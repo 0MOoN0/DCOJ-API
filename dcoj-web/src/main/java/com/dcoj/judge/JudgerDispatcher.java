5d3bea2454593670f78a961cc28060706cd9ec8e
0MOoN0 <814429032@qq.com>
2019-05-21 12:35:15 +0800

    [UPDATE(已测试)]修改判卷机调度信息，选择远程判卷机

--- dcoj-web/src/main/java/com/dcoj/judge/JudgerDispatcher.java
+++ dcoj-web/src/main/java/com/dcoj/judge/JudgerDispatcher.java
@@ -11,7 +11,7 @@ public class JudgerDispatcher {
 
     // 获取判卷URL
     public String getJudgerUrl() {
-        return "http://192.168.199.138:5000";
+        return "http://167.179.92.37:5000";
     }
 
 
